48ad4d91895c3c83ee48f957d3d6e5118dc2476c
Ryan Diel <rdiel01@gmail.com>
2018-10-07 15:44:28 -0500

    Initial commit. Created User & Blog classes.

---
+++ main.py
@@ -0,0 +1,63 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:**B1r2y3B$$@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(2000))
+    owner_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+
+    def __init__(self, title, body, owner):
+        self.title = title
+        self.body = body
+        self.owner_id = owner
+
+class User(db.Model):
+    
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(40), unique=True)
+    password = db.Column(db.String(30))
+    blogs = db.relationship('Task', backref='owner')
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+
+    if request.method == 'POST':
+        task_name = request.form['task']
+        new_task = Task(task_name)
+        db.session.add(new_task)
+        db.session.commit()
+
+    tasks = Task.query.filter_by(completed=False).all()
+    completed_tasks = Task.query.filter_by(completed=True).all()
+    return render_template('todos.html',title="Get It Done!", 
+        tasks=tasks, completed_tasks=completed_tasks)
+
+
+@app.route('/delete-task', methods=['POST'])
+def delete_task():
+
+    task_id = int(request.form['task-id'])
+    task = Task.query.get(task_id)
+    task.completed = True
+    db.session.add(task)
+    db.session.commit()
+
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run()
